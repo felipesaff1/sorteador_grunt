a0180eb4ae4c6574172a5c0ed2715d623c74b6bc
Felipe Andrade <felipe_saff1@hotmail.com>
2025-04-09 18:47:37 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,125 @@
+const { option } = require("grunt");
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css' : 'src/styles/main.less'
+                }
+            },
+            production: {
+                option: {
+                    Compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css' : 'src/styles/main.less'
+                }
+                
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            html:{
+                files:['src/index.html'],
+                tasks:['replace:dev']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns:[
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns:[
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+            ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace:true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild'],
+        uglify: {
+            tagert: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
+            }
+        }
+        /* Usar apenas para se for usar sass
+        sass: {
+            dist: {
+                options:{
+                    style: 'compressed'
+                },
+                files:{
+                    'main2.css' : 'main.scss'
+                }
+            }
+        },
+        concurrent: {
+            target: ['olaGrunt', 'less','sass']
+        }*/
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //Usar tasks abaixo apenas para uso do sass
+  //  grunt.loadNpmTasks('grunt-contrib-sass');
+ //   grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+};
